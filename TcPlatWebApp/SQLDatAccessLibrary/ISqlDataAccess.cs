353d409372de8fd8cd0ece032ac25066a04ce5a8
adriofdo <98824194+adriofdo@users.noreply.github.com>
2024-07-14 12:32:42 +0200

    init2adrio

---
+++ TcPlatWebApp/SQLDatAccessLibrary/ISqlDataAccess.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SQLDatAccessLibrary
+{
+    public interface ISqlDataAccess
+    {
+        string ConnectionString { get; set; }
+
+        Task<List<T>> LoadData<T, U>(string sql, U parameters);
+    }
+}
---
+++ TcPlatWebApp/SQLDatAccessLibrary/SqlDataAccess.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+using Mysqlx.Connection;
+
+namespace SQLDatAccessLibrary
+
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+        public string ConnectionString { get; set; } = "Server=servertc-25c772c3-adriotcplat2024.a.aivencloud.com;Port=20877;Database=Tcplat;Uid=avnadmin;Pwd=AVNS_ohq66m2-xD5dt9ouwg8;SSL Mode=Required;CertificateFile=ca.pem";
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters)
+        {
+            using (IDbConnection connection = new MySqlConnection(ConnectionString))
+            {
+                var data = await connection.QueryAsync<T>(sql, parameters);
+                return data.ToList();
+            }
+        }
+    }
+}
